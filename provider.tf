91ce535c05f327b659ebee6301b8708e08afd70e
Varunsimha Premanarasimha <varunsimha.premanarasimha@stratogent.com>
2025-12-09 01:31:39 +0530

    update

--- provider.tf
+++ provider.tf
@@ -1,7 +1,13 @@
 provider "aws" {
     region = "ap-southeast-1"
-    assume_role {
-      role_arn = ""
-      session_name = "terraform"
-    }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-devops-backend-file"
+    key = "terraform.tfstate"
+    region = "ap-southeast-1"
+    encrypt = true
+    use_lockfile = true
+  }
 }
