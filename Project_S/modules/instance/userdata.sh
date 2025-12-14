ce076e1d48347e5be8a9c2ab0043ecaf80457f57
Varunsimha Premanarasimha <varunsimha.premanarasimha@stratogent.com>
2025-12-14 11:57:05 +0530

    final code

--- Project_S/modules/instance/userdata.sh
+++ Project_S/modules/instance/userdata.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Update packages
-yum update -y
+apt update
+
+apt upgrade -y
 
 # Install Apache and mod_ssl
-yum install -y httpd mod_ssl
+yum install -y apache2
 
 # Start and enable Apache
-systemctl start httpd
-systemctl enable httpd
+systemctl start apache2
+systemctl enable apache2
 
 # Restart Apache
-systemctl restart httpd
+systemctl restart apache2
